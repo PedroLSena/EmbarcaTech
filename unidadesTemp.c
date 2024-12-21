0b2d28661d8aa652537d5e11d4a8481d236520e4
davi-aguiar <daviaguiar2010@hotmail.com>
2024-12-20 22:29:23 -0300

    :recycle: refactor: função main removida para modularização

--- unidadesTemp.c
+++ unidadesTemp.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// Funções de conversão de temperatura
 void celsiusToFahrenheitAndKelvin(double celsius)
 {
   double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
   double kelvin = celsius + 273.15;
-  printf("\n%.2f °C = %.2f °F = %.2f K\n", celsius, fahrenheit, kelvin);
+  printf("\n%.2f \u00b0C = %.2f \u00b0F = %.2f K\n", celsius, fahrenheit, kelvin);
 }
 
 void fahrenheitToCelsiusAndKelvin(double fahrenheit)
 {
   double celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
   double kelvin = celsius + 273.15;
-  printf("\n%.2f °F = %.2f °C = %.2f K\n", fahrenheit, celsius, kelvin);
+  printf("\n%.2f \u00b0F = %.2f \u00b0C = %.2f K\n", fahrenheit, celsius, kelvin);
 }
 
 void kelvinToCelsiusAndFahrenheit(double kelvin)
 {
   double celsius = kelvin - 273.15;
   double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
-  printf("\n%.2f K = %.2f °C = %.2f °F\n", kelvin, celsius, fahrenheit);
+  printf("\n%.2f K = %.2f \u00b0C = %.2f \u00b0F\n", kelvin, celsius, fahrenheit);
 }
 
-int main()
+// Função para converter temperatura
+void converterTemperatura()
 {
   int choice;
   double temperature;
 
-  printf("Conversor de Temperatura\n");
+  printf("\nConversor de Temperatura\n");
   printf("1. Celsius para Fahrenheit e Kelvin\n");
   printf("2. Fahrenheit para Celsius e Kelvin\n");
   printf("3. Kelvin para Celsius e Fahrenheit\n");
-  printf("Escolha uma opção (1-3): ");
+  printf("Escolha uma op\u00e7\u00e3o (1-3): ");
   scanf("%d", &choice);
 
   switch (choice)
@@ -51,9 +54,7 @@ int main()
     kelvinToCelsiusAndFahrenheit(temperature);
     break;
   default:
-    printf("\nOpção inválida!\n");
+    printf("\nOp\u00e7\u00e3o inv\u00e1lida!\n");
     break;
   }
-
-  return 0;
 }
